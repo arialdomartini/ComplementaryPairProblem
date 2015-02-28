70979cb580ccd7ca14266d46c2ade5a5708465e0
Arialdo Martini <arialdomartini@gmail.com>
2015-03-01 00:07:43 +0100

    Solution O(n^2) with a double for cycle

---
+++ ComplementaryPairs.Test/SolutionTest.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace ComplementaryPairs.Test
+{
+    public class SolutionTest
+    {   
+	    [TestCase(10, new[]{1,2,3,4}, 0)]
+	    [TestCase(20, new[]{10,2,3,4}, 1)]
+	    [TestCase(10, new[]{5, 5}, 4)]
+	    [TestCase(10, new[]{1, 5, 9}, 3)]
+		public void ShouldFindTheNumberOfPairs(int target, int[] array, int expectedNumberOfPairs)
+	    {
+		    new BruteForceSolution().Solve(target, array).Should().Be(expectedNumberOfPairs);
+	    }
+    }
+}
---
+++ ComplementaryPairs/BruteForceSolution.cs
@@ -0,0 +1,22 @@
+﻿namespace ComplementaryPairs
+{
+	public class BruteForceSolution
+	{
+		public int Solve(int target, int[] array)
+		{
+			var count = 0;
+			foreach (var element in array)
+			{
+				foreach (var other in array)
+				{
+					if (element + other == target)
+					{
+						count++;
+					}
+				}
+			}
+			return count;
+		}
+	}
+}
+ 
